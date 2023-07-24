785abe9e7f9fbfbf023214a857834b55548bb2b4
selinnoz <73435487+selinnoz@users.noreply.github.com>
2023-07-24 04:24:49 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//1.Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n).
+//Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.
+
+//2.Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m).
+//Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
+
+//3.Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n).
+//Sonrasında kullanıcıdan n adet kelime girmesi isteyin. Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.
+
+//4.Bir konsol uygulamasında kullanıcıdan bir cümle yazması isteyin. Cümledeki toplam kelime ve harf sayısını console'a yazdırın.
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Give me a number:");
+            int n = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Now give me " + n + " number:");
+            int[] numbers = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine( i+1 +" . number");
+                numbers[i] = int.Parse(Console.ReadLine());
+                
+            }
+            Console.WriteLine("Even numbers:");
+            for (int i = 0; i < n; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    Console.WriteLine(numbers[i]);
+                }
+            }
+
+            Console.Read();
+
+        }
+        
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//2.Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m).
+//Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter 2 positive numbers!");        
+           // Console.WriteLine("1-");
+            int n= Convert.ToInt16(Console.ReadLine());               // n sayisi alinir
+           // Console.WriteLine("2-");
+            int m = Convert.ToInt16(Console.ReadLine());              // m sayisi alinir
+
+            Console.WriteLine("Please enter "+ n + " positive numbers!");         // kullanicidan n adet sayi istenir
+
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)                          // n adet sayi bir dizide tutulur
+            {
+                a[i] = Convert.ToInt16(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (a[i] % m == 0)
+                {
+                    Console.WriteLine(a[i] + " divided by " + m + " equals 0. Complete division!"); //girilen sayilardan m' e tam bölünenler
+                }
+                else
+                    Console.WriteLine(a[i] + " can't divided by " + m + "!");      //m'e bölünemeyen sayilar
+                if (a[i]  == m )
+                {
+                    Console.WriteLine(a[i] + " is equal to " + m + "!");       //girilen sayilardan m e esit olanlar
+                }
+            }
+
+
+            Console.Read();
+        }
+    }
+}
